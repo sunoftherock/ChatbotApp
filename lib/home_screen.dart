ab4129960326dfe56fb1f8df402251715d2a7ff6
Rocky <me.rocky.sun@gmail.com>
2021-02-06 16:27:00 -0500

    new files added as well

---
+++ lib/home_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'matches.dart';
+import 'dart:developer';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              'lib/assets/images/pearicon.png',
+              fit: BoxFit.contain,
+              height: 32,
+            ),
+            Container(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text('Matches'))
+          ],
+        ),
+      ),
+      body: Center(
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: Icon(Icons.account_box_rounded),
+                title: Text('hi'),
+                subtitle: Text('Last online 34 minutes ago'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: const Text('UNMATCH'),
+                    onPressed: () {/* ... */},
+                  ),
+                  const SizedBox(width: 8),
+                  TextButton(
+                    child: const Text('MESSAGE'),
+                    onPressed: () {/* ... */},
+                  ),
+                  const SizedBox(width: 8),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/matches.dart
@@ -0,0 +1,6 @@
+class Matches {
+  static final getMatches = [
+    {'name': 'Rocky', 'status': 'Online', 'bonjour': 'hello'},
+    {'name': 'Allen', 'status': 'Last seen 69 minutes ago'}
+  ];
+}
